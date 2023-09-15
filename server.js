603d1386e2d11c79284631d7f280718d73364e4c
Kamilucha <brnkamila2@gmail.com>
2023-09-15 20:22:18 +0200

    add config

--- server.js
+++ server.js
@@ -1,15 +1,10 @@
 const app = require('./app')
 
-// app.listen(3000, () => {
-//   console.log("Server running. Use our API on port: 3000")
-// })
-
 const mongoose = require("mongoose");
 
-const DB_HOST = "mongodb+srv://Kamila:11111111k@cluster0.fib0ay0.mongodb.net/db_contacts?retryWrites=true&w=majority";
+const { DB_HOST } = process.env;
 
 mongoose.set('strictQuery', true)
-
 
 mongoose.connect(DB_HOST)
   .then(() => {
