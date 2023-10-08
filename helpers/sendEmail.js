ccb2a86d0407f1c0f8777b1a0829f866d8cb472c
Kamilucha <brnkamila2@gmail.com>
2023-10-08 20:54:45 +0300

    1st comm

---
+++ helpers/sendEmail.js
@@ -0,0 +1,39 @@
+const ElasticEmail = require('@elasticemail/elasticemail-client');
+require("dotenv").config();
+
+const { ELASTIC_API_KEY, EMAIL_FROM } = process.env;
+
+const defaultClient = ElasticEmail.ApiClient.instance;
+ 
+const {apikey} = defaultClient.authentications;
+apikey.apiKey = ELASTIC_API_KEY;
+
+const api = new ElasticEmail.EmailsApi();
+
+const sendEmail = ElasticEmail.EmailMessageData.constructFromObject({
+  Recipients: [
+    new ElasticEmail.EmailRecipient("lafic23473@viperace.com")
+  ],
+  Content: {
+    Body: [
+      ElasticEmail.BodyPart.constructFromObject({
+        ContentType: "HTML",
+        Content: "<strong>Test email</strong>"
+      })
+    ],
+    Subject: "Test email",
+    From: EMAIL_FROM
+  }
+});
+ 
+const callback = function(error, data, response) {
+  if (error) {
+    console.error(error.message);
+  } else {
+    console.log('API called successfully.');
+  }
+};
+
+api.emailsPost(sendEmail, callback);
+
+module.exports = sendEmail;
